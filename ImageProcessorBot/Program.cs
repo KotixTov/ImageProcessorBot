0461a0b38b813bc7503f8c178eb44b94d0626b51
Leningradets <41923218+Leningradets@users.noreply.github.com>
2022-04-22 08:58:45 +0300

    Initial commit

---
+++ ImageProcessorBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageProcessorBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
